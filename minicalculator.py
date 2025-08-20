39cf600a15b303cf5face0764d224c6eebd740cf
Aswinprasath <gowthamginju9524@gmail.com>
2025-08-20 06:28:12 +0530

    modified minicalculator.py

--- minicalculator.py
+++ minicalculator.py
@@ -1,21 +1,21 @@
 print("            It's Mini Calculator              ")
 
-print("If you want to Calculate two numbers by Adding, Subracting Multiple or Divisible. Here, you must type '1'. ")
+print("If you want to Calculate two numbers by Adding, Subracting Multiple or Divisible. \n Here, you must type '1':  \r ")
 a=int(input())
 if(a==1):
     print("Okay, Done.")
     print("Enter two numbers for A and B ")
     A = int(input("A :"))
-    B = int(input("B :"))
+    B =int(input("B :"))
     C = input("Choose your Operator : 'add,sub,mul,div' Then Enter : ")
     if(C == "add"):
-        print(A+B)
+        print("Answer = ", A+B)
     elif(C == "sub"):
-        print(A-B)
+        print("Answer = ", A-B)
     elif(C == "mul"):
-        print(A*B)
+        print("Answer = ", A*B)
     elif(C == "div"):
-        print(A/B)
+        print("Answer = ", A/B)
     else:
         print("Sorry, You didn't choose the correct choice.")
 else:
